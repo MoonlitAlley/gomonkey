e011b31f796cff76422ab7ac7672a93f8a0514c1
MoonlitAlley <46488569+MoonlitAlley@users.noreply.github.com>
2021-09-17 13:07:12 +0800

    Update modify_binary_darwin.go
    feat: support macOs Big Sur

--- modify_binary_darwin.go
+++ modify_binary_darwin.go
@@ -6,7 +6,7 @@ func modifyBinary(target uintptr, bytes []byte) {
     function := entryAddress(target, len(bytes))
     
     page := entryAddress(pageStart(target), syscall.Getpagesize())
-    err := syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE|syscall.PROT_EXEC)
+    err := syscall.Mprotect(page, syscall.PROT_READ|syscall.PROT_WRITE)
     if err != nil {
         panic(err)
     }
